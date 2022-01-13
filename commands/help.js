b7c7c09f762cd7484775027d8d82438413c8251b
jack doggo <95576436+chaijack123gt@users.noreply.github.com>
2022-01-13 21:55:56 +0800

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.execute = async (client, message, args) => {
+    const embed = new MessageEmbed()
+        .setAuthor("Commands")
+        .setTitle("tap me")
+        .setURL("https://www.youtube.com/channel/UCsAhh2iG9oH1wq01MJv3ypg")
+        .setDescription(`Total Commands: ${client.commands.size}`)
+        .setColor("BLURPLE")
+        .setTimestamp()
+        .setThumbnail(client.user.displayAvatarURL)
+        .setFooter(message.author.tag, message.author.displayAvatarURL);
+    client.commands.forEach(cmd => {
+        embed.addField(`${cmd.help.name}`, `Aliases: ${cmd.help.aliases.join(", ") || "None"}\nUsage: \`${client.prefix}${cmd.help.usage}\``, true);
+    });
+    return message.channel.send(embed);
+}
+
+exports.help = {
+    name: "help",
+    aliases: ["h"],
+    usage: `help`
+}
