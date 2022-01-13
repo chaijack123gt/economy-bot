238c3e73b047f0b98ec13cd3deca3299cb4382a6
jack doggo <95576436+chaijack123gt@users.noreply.github.com>
2022-01-13 21:41:38 +0800

    giveaway bot
    an discord bot

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "token",
+    prefix: "your prefix",
+    admins: [
+        "your id"
+],
+    debug: true,
+    countChannel: "923424240672317505"
+};
---
+++ index.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ disableMentions: 'everyone' });
+const Eco = require("quick.eco");
+client.eco = new Eco.Manager(); // quick.eco
+client.db = Eco.db; // quick.db
+client.config = require("./botConfig");
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.shop = {
+  diamon: {
+    cost: 1000
+  },
+  gold: {
+    cost: 2000
+  },
+  pc: {
+    cost: 3000
+  },
+  sub_jack_doggo: {
+    cost: 1
+  }
+};
+const fs = require("fs");
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    const event = require(`./events/${f}`);
+    let eventName = f.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    let command = require(`./commands/${f}`);
+    client.commands.set(command.help.name, command);
+    command.help.aliases.forEach(alias => {
+      client.aliases.set(alias, command.help.name);
+    });
+  });
+});
+
+
+client.login(client.config.token);
